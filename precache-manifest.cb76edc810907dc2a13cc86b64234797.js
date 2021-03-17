3d175804c0826ef0eaf6867169d67d9c59e82965
Lionyu <779625444@qq.com>
2021-03-17 12:12:59 +0800

    deploy

---
+++ precache-manifest.cb76edc810907dc2a13cc86b64234797.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a154ec2c909f48a6d1a",
+    "url": "/financier-1-website/css/app.343a9993.css"
+  },
+  {
+    "revision": "72a907c4ace087c44e12c213545312b4",
+    "url": "/financier-1-website/index.html"
+  },
+  {
+    "revision": "7a154ec2c909f48a6d1a",
+    "url": "/financier-1-website/js/app.b3fbe073.js"
+  },
+  {
+    "revision": "b142de839b32c7a5df08",
+    "url": "/financier-1-website/js/chunk-vendors.ce42232d.js"
+  },
+  {
+    "revision": "2a3ddb2c81f4ff67f860726021d9f9e9",
+    "url": "/financier-1-website/manifest.json"
+  },
+  {
+    "revision": "87f3a16b12705bf0e3a7413bb8f021e1",
+    "url": "/financier-1-website/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/financier-1-website/robots.txt"
+  }
+]);
